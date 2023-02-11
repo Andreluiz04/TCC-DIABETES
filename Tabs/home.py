b56eaec9443e95a484a4a0eedc632cd3ffb1834e
Andreluiz04 <andre.luiz_1985@hotmail.com>
2023-02-11 20:54:49 -0300

    PAGINA INICIAL

---
+++ Tabs/home.py
@@ -0,0 +1,11 @@
+"""Este modulo tem dados sobre a pagina inicial"""
+
+#importando modulos necessarios
+import streamlit as st
+
+def app():
+	#adicionando titulo do trabalho
+	st.title("TCC sobre Diabetes")
+
+	#adicionar uma imagem na pagina inicial
+	st.image("./Imagens/inicial.JFIF")
